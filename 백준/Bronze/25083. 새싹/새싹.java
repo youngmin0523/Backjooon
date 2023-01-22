97a68e06494456295f86ff190310d676061ac99d
youngmin0523 <123279529+youngmin0523@users.noreply.github.com>
2023-01-22 20:56:37 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11240 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7\n").append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n").append("   `~\\/\n");
+		sb.append("      |\n").append("      |");
+		System.out.print(sb);
+	}
+}
