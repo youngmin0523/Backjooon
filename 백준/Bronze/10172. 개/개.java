4d1f59e5b43c145a178ff86752117ca07d03aa2c
youngmin0523 <123279529+youngmin0523@users.noreply.github.com>
2023-01-22 20:56:23 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11236 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|\n").append("|q p|   /}\n").append("( 0 )\"\"\"\\\n");
+		sb.append("|\"^\"`    |\n").append("||_/=\\\\__|");
+		System.out.print(sb);
+	}
+}
