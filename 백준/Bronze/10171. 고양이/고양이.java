b2d93f2e959e06f6711f6274264a7a6c4ed2c160
youngmin0523 <123279529+youngmin0523@users.noreply.github.com>
2023-01-22 20:56:13 +0900

    [Bronze V] Title: 고양이, Time: 76 ms, Memory: 11200 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
