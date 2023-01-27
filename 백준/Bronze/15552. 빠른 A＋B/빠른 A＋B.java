c2b199904495a199b20278bef4418910787c08c2
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-01-28 07:33:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 716 ms, Memory: 241564 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
