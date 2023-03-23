70f64a7874e0583b782d5a71a2d177d415dcce51
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-03-23 22:38:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
