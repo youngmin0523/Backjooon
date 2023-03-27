f236709609c859b86ba588c5985220886f11db7a
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-03-28 00:26:23 +0900

    [Silver V] Title: 창문 닫기, Time: 76 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println((int)Math.sqrt(N));
+        
+    }
+}
