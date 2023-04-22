e38b487ff9cfb8cb75645ad815e19c3635954521
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-04-22 22:19:46 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int arr[] = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int cnt = 0;
+		for(int i = N-1; i >= 0; i--) {
+			if(K >= arr[i]) {
+				cnt += K / arr[i];
+				K %= arr[i];
+			}
+		}
+		System.out.println(cnt);
+	}
+}
