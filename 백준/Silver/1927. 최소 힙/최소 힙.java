3faade7c3584a1995063d597789e39d8213c82f2
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-04-30 22:43:43 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 25420 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -5,21 +5,23 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for(int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
 			if(x == 0) {
 				if(pq.isEmpty()) {
-					System.out.println(0);
+					sb.append(0).append('\n');
 				}
 				else {
-					System.out.println(pq.poll());
+					sb.append(pq.poll()).append('\n');
 				}
 			}
 			else {
 				pq.offer(x);
 			}
 		}
+		System.out.println(sb.toString());
 	}
 }
