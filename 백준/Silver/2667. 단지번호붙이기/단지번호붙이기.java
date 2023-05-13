0d3b8d9c98769cdb4e2980e2b84ac5148693317b
youngmin Hong <123279529+youngmin0523@users.noreply.github.com>
2023-05-13 17:56:44 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 11764 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+	
+	static class Pos{
+		int r, c;
+		
+		Pos(int r, int c){
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static int N;
+	static int map[][];
+	static boolean visited[][];
+	static List<Integer> list;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		list = new ArrayList<>();
+		int cnt = 0;
+		
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for(int j = 0; j < N; j++) {
+				map[i][j] = str.charAt(j)-'0';
+			}
+		}
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(!visited[i][j] && map[i][j] == 1) {
+					cnt++;
+					list.add(bfs(i, j));
+				}
+			}
+		}
+		System.out.println(cnt);
+		Collections.sort(list);
+		for(int it : list) {
+			System.out.println(it);
+		}
+	}
+	static int bfs(int r, int c) {
+		Queue<Pos> q = new ArrayDeque<>();
+		q.offer(new Pos(r, c));
+		visited[r][c] = true;
+		int cnt = 1;
+		
+		Pos curr;
+		while(!q.isEmpty()) {
+			curr = q.poll();
+			
+			int nr, nc;
+			for(int d = 0; d < 4; d++) {
+				nr = curr.r + dr[d];
+				nc = curr.c + dc[d];
+				
+				if(range(nr, nc) && !visited[nr][nc] && map[nr][nc] == 1) {
+					visited[nr][nc] = true;
+					cnt++;
+					q.offer(new Pos(nr, nc));
+				}
+			}
+		}
+		return cnt;
+	}
+	static boolean range(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
+}
